0ef9e4e433f522678e97b55538e3454a28fc6596
Kamilalk <03KLKrukowska@gmail.com>
2022-07-12 12:31:48 +0200

    registration service

--- src/main/java/Krukowska/UserLogin/demo/registration/RegistrationService.java
+++ src/main/java/Krukowska/UserLogin/demo/registration/RegistrationService.java
@@ -1,8 +1,31 @@
 package Krukowska.UserLogin.demo.registration;
 
+import Krukowska.UserLogin.demo.appuser.AppUser;
+import Krukowska.UserLogin.demo.appuser.AppUserRole;
+import Krukowska.UserLogin.demo.appuser.AppUserService;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
+
+    private AppUserService appUserService;
+    private EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator
+                .test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 
 }
